89bc982402aecb54272e9c87c1fc4d639784d615
marshall-strong <marshallstrong123@gmail.com>
2022-07-20 13:41:50 -0700

    Comment out the elephant search buttons used for testing

--- pexels-photo-search/src/SearchForPhotos.jsx
+++ pexels-photo-search/src/SearchForPhotos.jsx
@@ -2,26 +2,26 @@ import React, { useState } from "react";
 import "./SearchForPhotos.css";
 
 const SearchForPhotos = ({ setNextUrl }) => {
-  const searchForElephants = () =>
-    setNextUrl(
-      "https://api.pexels.com/v1/search/?page=1&per_page=10&query=elephants"
-    );
-
   const [userInput, setUserInput] = useState("");
   const [encodedURI, setEncodedURI] = useState("");
-
+  
   const handleUserInput = (event) => {
     setUserInput(event.target.value);
     const uri = `https://api.pexels.com/v1/search/?page=1&per_page=10&query=${userInput}`;
     const encoded = encodeURI(uri);
     setEncodedURI(encoded);
   };
-
+  
   const handleSubmit = (event) => {
     event.preventDefault();
     setNextUrl(encodedURI);
     console.log('search form submitted');
   };
+
+  // const searchForElephants = () =>
+  //   setNextUrl(
+  //     "https://api.pexels.com/v1/search/?page=1&per_page=10&query=elephants"
+  //   );
 
   return (
     <div className="SearchForPhotos">
@@ -38,7 +38,7 @@ const SearchForPhotos = ({ setNextUrl }) => {
         </button>
       </form>
       <button onClick={handleSubmit}>Search for Photos</button>
-      <button onClick={searchForElephants}>Search for Elephants</button>
+      {/* <button onClick={searchForElephants}>Search for Elephants</button> */}
     </div>
   );
 };
