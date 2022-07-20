9c313a9fa48b66a026acf08caa309f32f019912e
marshall-strong <marshallstrong123@gmail.com>
2022-07-20 11:54:42 -0700

    MVP goal met! Photo Search feature:
    As a user types in the search bar, the `onChange` event handler updates the value of `userInput` in the `SearchForPhotos` component state.
    The `onChange` event handler also generates an (unencoded) query string for the API request, then uses the `encodeURI` function to escape any special characters in the user input.
    When the submit button is clicked, the `setNextUrl` method from the `Gallery` component updates the value of `nextUrl` in state, then submits a new async API request.

--- pexels-photo-search/src/SearchForPhotos.jsx
+++ pexels-photo-search/src/SearchForPhotos.jsx
@@ -1,9 +1,21 @@
-import React from "react";
+import React, { useState } from "react";
 import "./SearchForPhotos.css";
 
 const SearchForPhotos = ({ setNextUrl }) => {
   const searchForElephants = () =>
     setNextUrl("https://api.pexels.com/v1/search?query=elephants&per_page=10");
+
+  const [userInput, setUserInput] = useState(null);
+  const [encodedURI, setEncodedURI] = useState(null);
+
+  const handleUserInput = (event) => {
+    setUserInput(event.target.value);
+    const uri = `https://api.pexels.com/v1/search?query=${userInput}&per_page=10`;
+    const encoded = encodeURI(uri);
+    setEncodedURI(encoded);
+  };
+
+  const submitSearchRequest = () => setNextUrl(encodedURI);
 
   return (
     <div className="SearchForPhotos">
@@ -11,9 +23,13 @@ const SearchForPhotos = ({ setNextUrl }) => {
       <form id="search-for-photos">
         <input
           type="text"
+          value={userInput}
+          onChange={handleUserInput}
           placeholder="Enter a topic to search Pexels for relevant photos (ex: 'elephants on parade')"
         />
+        {/* <input type="submit" value="Find Photos" /> */}
       </form>
+      <button onClick={submitSearchRequest}>Search for Photos</button>
       <button onClick={searchForElephants}>Search for Elephants</button>
     </div>
   );
