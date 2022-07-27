cff25e1fb71f8b6330ba3c7548fd715bdfa52bfe
marshall-strong <marshallstrong123@gmail.com>
2022-07-27 13:03:40 -0700

    update `Footer` styling and layout

--- pexels-photo-search/src/Footer.jsx
+++ pexels-photo-search/src/Footer.jsx
@@ -1,12 +1,29 @@
 import React from "react";
 import "./Footer.css";
 import pexelsLogo from "./img/pexelsLogo.svg"
+import pexelsBlackLogo from "./img/pexelsBlackLogoOnTransparent.png";
+import pexelsColoredLogo from "./img/pexelsLogoOnTransparent.png";
+import pexelsWhiteLogo from "./img/pexelsWhiteLogoOnTransparent.png";
 
 const Footer = () => {
   return (
     <div className="Footer">
-      <img src={pexelsLogo} className="pexelsLogo" alt="Powered by Pexels" />
-      <div>Powered by Pexels</div>
+      <div className="leftNav">
+        <div className="imgWrapper">
+          <img
+            src={pexelsColoredLogo}
+            className="pexelsLogo"
+            alt="Powered by Pexels"
+          />
+        </div>
+      </div>
+      <div className="centerNav">
+        <div className="copyright">Copyright © 2022 Marshall Strong</div>
+      </div>
+      <div className="rightNav">
+        <img src={pexelsLogo} className="pexelsLogo" alt="Powered by Pexels" />
+        <div>Powered by Pexels</div>
+      </div>
     </div>
   );
 };
