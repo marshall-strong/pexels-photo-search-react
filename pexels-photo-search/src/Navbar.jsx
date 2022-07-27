a36be8b3310d9540246d0207a293d55e7d6522ae
marshall-strong <marshallstrong123@gmail.com>
2022-07-27 12:39:42 -0700

    update `Navbar` styling and layout

--- pexels-photo-search/src/Navbar.jsx
+++ pexels-photo-search/src/Navbar.jsx
@@ -1,18 +1,27 @@
 import React from "react";
 import "./Navbar.css";
+import pexelsBlackLogo from "./img/pexelsBlackLogoOnTransparent.png";
+import pexelsColoredLogo from "./img/pexelsLogoOnTransparent.png";
+import pexelsWhiteLogo from "./img/pexelsWhiteLogoOnTransparent.png";
 
 const Navbar = () => {
   return (
     <div className="Navbar">
-      <div className="leftNav"></div>
+      <div className="leftNav">left nav</div>
       <div className="centerNav">
         <div className="navbarTitle">React Photo Search App</div>
         <div className="navbarSubtitle">
-          <span>Powered by </span>
-          Pexels
+          <div className="textWrapper">Powered by </div>
+          <div className="imgWrapper">
+            <img
+              src={pexelsColoredLogo}
+              className="pexelsLogo"
+              alt="Powered by Pexels"
+            />
+          </div>
         </div>
       </div>
-      <div className="rightNav"></div>
+      <div className="rightNav">right nav</div>
     </div>
   );
 };
