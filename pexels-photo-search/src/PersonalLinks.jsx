d293bb1cc976ce062b90cc2eb8f67f6c7dd944f4
marshall-strong <marshallstrong123@gmail.com>
2022-07-27 14:30:02 -0700

    update Personal Links in `Footer`

--- pexels-photo-search/src/PersonalLinks.jsx
+++ pexels-photo-search/src/PersonalLinks.jsx
@@ -1,75 +1,77 @@
 import React from "react";
+import headshotIcon from "./img/headshot-512.png";
 import emailIcon from "./img/emailIcon.svg"
 import githubIcon from "./img/githubIcon.svg"
 import linkedinIcon from "./img/linkedinIcon.svg";
+import resumeIcon from "./img/resumeIcon.svg"
 import "./PersonalLinks.css";
 
 const PersonalLinks = () => {
   return (
     <div className="PersonalLinks">
       {/* <div className="col-sm-5 social"> */}
-        <ul>
-          <li>
-            <a
-              title="marshallstrong.com"
-              className="btn-rounded-white"
-              href="https://marshallstrong.com/"
-              rel="noopener noreferrer"
-              target="_blank"
-            >
-              <img
-                src={linkedinIcon}
-                className="linkedinIcon"
-                alt="LinkedIn profile"
-              />
-            </a>
-          </li>
-          <li>
-            <a
-              title="LinkedIn"
-              className="btn-rounded-white"
-              href="https://www.linkedin.com/in/marshall-strong/"
-              rel="noopener noreferrer"
-              target="_blank"
-            >
-              <img
-                src={linkedinIcon}
-                className="linkedinIcon"
-                alt="LinkedIn profile"
-              />
-            </a>
-          </li>
-          <li>
-            <a
-              title="GitHub"
-              className="btn-rounded-white"
-              href="https://github.com/marshall-strong"
-              rel="noopener noreferrer"
-              target="_blank"
-            >
-              <img
-                src={githubIcon}
-                className="githubIcon"
-                alt="GitHub repository"
-              />
-            </a>
-          </li>
-          <li>
-            <a
-              title="resume"
-              className="btn-rounded-white"
-              href="https://drive.google.com/file/d/1OhmDMX02cyVx8ph7CV4-rnyVGrw1Y8dk/view?usp=sharing"
-              rel="noopener noreferrer"
-              target="_blank"
-            >
-              <img
-                src={githubIcon}
-                className="resumeIcon"
-                alt="Resume for Marshall Strong"
-              />
-            </a>
-          </li>
-        </ul>
+      <ul>
+        <li>
+          <a
+            title="Portfolio Site"
+            className="btn-rounded-white"
+            href="https://marshallstrong.com/"
+            rel="noopener noreferrer"
+            target="_blank"
+          >
+            <img
+              src={headshotIcon}
+              className="portfolioIcon socialIcon"
+              alt="Development Portfolio for Marshall Strong"
+            />
+          </a>
+        </li>
+        <li>
+          <a
+            title="LinkedIn Profile"
+            className="btn-rounded-white"
+            href="https://www.linkedin.com/in/marshall-strong/"
+            rel="noopener noreferrer"
+            target="_blank"
+          >
+            <img
+              src={linkedinIcon}
+              className="linkedinIcon socialIcon"
+              alt="LinkedIn Profile for Marshall Strong"
+            />
+          </a>
+        </li>
+        <li>
+          <a
+            title="GitHub repository"
+            className="btn-rounded-white"
+            href="https://github.com/marshall-strong"
+            rel="noopener noreferrer"
+            target="_blank"
+          >
+            <img
+              src={githubIcon}
+              className="githubIcon socialIcon"
+              alt="GitHub repository"
+            />
+          </a>
+        </li>
+        <li>
+          <a
+            title="Marshall Strong's Resume"
+            className="btn-rounded-white"
+            href="https://drive.google.com/file/d/1OhmDMX02cyVx8ph7CV4-rnyVGrw1Y8dk/view?usp=sharing"
+            rel="noopener noreferrer"
+            target="_blank"
+          >
+            <img
+              src={resumeIcon}
+              className="resumeIcon socialIcon"
+              alt="Resume for Marshall Strong"
+            />
+          </a>
+        </li>
+      </ul>
       {/* </div> */}
     </div>
   );
