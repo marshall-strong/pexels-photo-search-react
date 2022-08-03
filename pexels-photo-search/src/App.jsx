0f66125f29148e9480ab607f1c054bf55e2c062f
marshall-strong <marshallstrong123@gmail.com>
2022-08-03 13:30:15 -0700

    clean up code and comments left over from refactoring

--- pexels-photo-search/src/App.jsx
+++ pexels-photo-search/src/App.jsx
@@ -7,48 +7,6 @@ import Gallery from "./Gallery";
 import Footer from "./Footer";
 import "./App.css";
 
-// const Gallery = ({ response, userInput, returnToHomepage }) => {
-//   return (
-//     <div className="Gallery">
-//       <div className="galleryContainer">
-//         {!response ? (
-//           <div>
-//             <p>Waiting on Pexels API response...</p>
-//           </div>
-//         ) : response.total_results <= 0 ? (
-//           <div>
-//             <p>
-//               Your search - <b>{userInput}</b> - did not match any photos.
-//             </p>
-//             <p>Suggestions:</p>
-//             <div>
-//               <ul>Make sure all words are spelled correctly.</ul>
-//               <ul>Try different keywords.</ul>
-//               <ul>Try more general keywords.</ul>
-//               <ul>Try fewer keywords.</ul>
-//             </div>
-//             <button onClick={returnToHomepage}>Return to Homepage</button>
-//           </div>
-//         ) : !response.photos ? (
-//           <div className="paginationContainer">
-//             <div className="galleryPhotos"></div>
-//           </div>
-//         ) : (
-//           <div className="paginationContainer">
-//             <div className="galleryPhotos">
-//               {response.photos.map((photoData, index) => (
-//                 <React.Fragment key={index}>
-//                   <Photo photoData={photoData} />
-//                 </React.Fragment>
-//               ))}
-//             </div>
-//           </div>
-//         )}
-//       </div>
-//     </div>
-//   );
-// };
-
 const App = () => {
     const [displayedUrl, setDisplayedUrl] = useState(null);
     const [newUrl, setNewUrl] = useState(null);
