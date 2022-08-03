c122f805f0a114a2c1e5d2f54605367befa3c6cb
marshall-strong <marshallstrong123@gmail.com>
2022-08-03 14:45:57 -0700

    add a "Return to Homepage" icon to the `Navbar`

--- pexels-photo-search/src/Navbar.jsx
+++ pexels-photo-search/src/Navbar.jsx
@@ -1,5 +1,6 @@
 import React from "react";
 import PersonalLinks from "./PersonalLinks";
+import homeIcon from "./img/homeIcon.svg";
 import pexelsColoredLogo from "./img/pexelsLogoOnTransparent.png";
 // import pexelsBlackLogo from "./img/pexelsBlackLogoOnTransparent.png";
 // import pexelsWhiteLogo from "./img/pexelsWhiteLogoOnTransparent.png";
@@ -13,10 +14,10 @@ const Navbar = ({ attributionUrl, returnToHomepage }) => {
         <div className="navbarTitle">React Photo Search App</div>
         <div className="navbarSubtitle">
           <a
-            title="Powered by Pexels"
             href={attributionUrl}
             rel="noopener noreferrer"
             target="_blank"
+            title="Powered by Pexels"
           >
             <div className="textWrapper">Powered by </div>
             <div className="imgWrapper">
@@ -30,7 +31,13 @@ const Navbar = ({ attributionUrl, returnToHomepage }) => {
         </div>
       </div>
       <div className="centerNav">
-        <button onClick={returnToHomepage}>Return to Homepage</button>
+        <img
+          alt="Return to Homepage"
+          className="icon"
+          onClick={returnToHomepage}
+          src={homeIcon}
+          title="Return to Homepage"
+        />
       </div>
       <div className="rightNav">
         <PersonalLinks />
