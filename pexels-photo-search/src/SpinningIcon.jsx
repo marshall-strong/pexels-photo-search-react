29559e8fb7d413adbafed37337cca0e2c96aab18
marshall-strong <marshallstrong123@gmail.com>
2022-08-05 13:06:17 -0700

    Update to `SpinningIcon` component -- React logo spins below a pulsar the same color as the Pexels logo, which sits on the very top. `SpinningIcon` can now be added to `Splash` section of homepage.

--- pexels-photo-search/src/SpinningIcon.jsx
+++ pexels-photo-search/src/SpinningIcon.jsx
@@ -1,11 +1,14 @@
 import React from "react";
 import reactIcon from "./img/reactIcon.svg";
+import pexelsIcon from "./img/pexelsCircle.svg"
 import "./SpinningIcon.css";
 
 const SpinningIcon = () => {
   return (
     <div className="SpinningIcon">
-      <img src={reactIcon} className="spinEffect" alt="a spinning animation" />
+      <img src={reactIcon} className="spinEffect reactIcon" alt="React logo" />
+      <div class="pulsar"></div>
+      <img src={pexelsIcon} className="pulseEffect pexelsIcon" alt="Pexels logo" />
     </div>
   );
 };
