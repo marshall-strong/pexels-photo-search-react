e9ee7dc18e770704471c7a8a264c8f78599b667f
marshall-strong <marshallstrong123@gmail.com>
2022-08-05 14:52:56 -0700

    clean up `Splash` component

--- pexels-photo-search/src/Splash.jsx
+++ pexels-photo-search/src/Splash.jsx
@@ -21,37 +21,17 @@ const Splash = ({ attributionUrl, returnToHomepage }) => {
   return (
     <div className="Splash">
       <div className="matrixLayout">
-        <ul className="row topRow">
-          <li className="col leftCol">
-            <span>&#x2196;</span>
-          </li>
-          <li className="col centerCol">
-            <span>&#x2191;</span>
-          </li>
-          <li className="col rightCol">
-            <span>&#x2197;</span>
-          </li>
-        </ul>
         <ul className="row middleRow">
           <li className="col leftCol">
-            <span>&#x2190;</span>
+            React Photo Search
+            {/* <span>&#x2190;</span> */}
           </li>
           <li className="col centerCol">
             <SpinningIcon />
           </li>
           <li className="col rightCol">
-            <span>&#x2192;</span>
-          </li>
-        </ul>
-        <ul className="row bottomRow">
-          <li className="col leftCol">
-            <span>&#x2199;</span>
-          </li>
-          <li className="col centerCol">
-            <span>&#x2193;</span>
-          </li>
-          <li className="col rightCol">
-            <span>&#x2198;</span>
+            Powered by Pexels
+            {/* <span>&#x2192;</span> */}
           </li>
         </ul>
       </div>
