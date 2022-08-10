3693150e0c30ab8fc6566b09f9efcfe00548c9a2
marshall-strong <marshallstrong123@gmail.com>
2022-08-09 20:43:30 -0700

    style the `SearchForm` component `<input type="text">` element: change the background color of the input field to "lightest"; change the color of the placeholder text to "darker"; when the user clicks inside the input field, change the placeholder text to "lightest" so that it disappears and only a cursor remains.

--- pexels-photo-search/src/SearchForm.jsx
+++ pexels-photo-search/src/SearchForm.jsx
@@ -22,7 +22,7 @@ const SearchForm = ({
     <svg
       xmlns="http://www.w3.org/2000/svg"
       viewBox="0 0 512 512"
-      className="icon enabled"
+      className="icon enabled iconSearch"
       onClick={handleSubmit}
       alt="Search"
       title="Search for Photos"
