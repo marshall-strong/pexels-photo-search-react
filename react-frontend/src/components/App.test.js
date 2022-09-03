6d5709af74300549a5fed8d593938f2e1834bbcb
marshall-strong <marshallstrong123@gmail.com>
2022-09-02 20:47:08 -0700

    first Jest test! A basic "smoke test" for the `<App/>` component.

--- react-frontend/src/components/App.test.js
+++ react-frontend/src/components/App.test.js
@@ -1,8 +1,26 @@
-import { render, screen } from "@testing-library/react";
+import React from "react";
+import { render, unmountComponentAtNode } from "react-dom";
 import App from "./App";
 
-test("renders learn react link", () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+// <https://reactjs.org/docs/testing-recipes.html>
+// <https://create-react-app.dev/docs/running-tests/#testing-components>
+
+// isolates each test by setting up before and cleaning up after
+let container = null;
+
+beforeEach(() => {
+  // setup a DOM element as the test's render target
+  container = document.createElement("div");
+  document.body.appendChild(container);
+});
+
+afterEach(() => {
+  unmountComponentAtNode(container);
+  container.remove();
+  container = null;
+});
+
+// basic "smoke test"
+it("renders without crashing", () => {
+  render(<App />, container);
 });
