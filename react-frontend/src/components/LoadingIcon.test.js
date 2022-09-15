3f3959259d3923f8715bebebfa72cdc223b81b53
marshall-strong <marshallstrong123@gmail.com>
2022-09-15 16:23:48 -0700

    Creates Jest testing files for all currently used components by using the test for the `App` component as a template (`App.test.js`)

---
+++ react-frontend/src/components/LoadingIcon.test.js
@@ -0,0 +1,28 @@
+import React from "react";
+import { createRoot } from "react-dom/client";
+import LoadingIcon from "./LoadingIcon";
+
+// <https://reactjs.org/docs/testing-recipes.html>
+// <https://create-react-app.dev/docs/running-tests/#testing-components>
+
+// isolates tests by setting up before and cleaning up after each test
+let container = null;
+let root = null;
+
+beforeEach(() => {
+  // setup a DOM element as the test's render target
+  container = document.createElement("div");
+  root = createRoot(container);
+});
+
+afterEach(() => {
+  root.unmount();
+  root = null;
+  container.remove();
+  container = null;
+});
+
+// basic "smoke test"
+it("renders without crashing", () => {
+  root.render(<LoadingIcon />);
+});
